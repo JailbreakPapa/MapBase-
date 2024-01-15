53af8decbdabea9e35f5bedff27f7020fc152c1f
Mikael K Aboagye <mike.amanfo12@gmail.com>
2024-01-14 23:37:53 -0500

    Add RML & VRML project

---
+++ sp/src/vrml/vrml.h
@@ -0,0 +1,20 @@
+//========= Mapbase++ - https://github.com/JailbreakPapa/MapBase- ============//
+//
+// Purpose: Custom implementation of RmlUi in Source 2013.
+// 
+// Author(s): JailbreakPapa (Mikael K. Aboagye)
+//
+// $NoKeywords: $
+//=============================================================================//
+
+#include "tier1/tier1.h"
+
+#include "RmlUi/Core.h"
+#include "RmlUi/Debugger.h"
+
+class IVRMLManager;
+class CRmlManager : public CTier1AppSystem<IVRMLManager>
+{
+public:
+	virtual 
+};
